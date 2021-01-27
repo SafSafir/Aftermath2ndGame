35900d5ab0b4179d2b645480dcd3ed7378c140e7
Ziya <47672872+SafSafir@users.noreply.github.com>
2021-01-27 21:31:12 +0300

    Mekaniğe başlandı

---
+++ CubeStack/Assets/Scripts/CubeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    enum CubeFieldStates
+    {
+        UpField,
+        RightField,
+        LeftField,
+        FrontField,
+        BackField
+    }
+
+    public GameObject cubePrefab;
+    
+
+
+    public Vector3 lastEndPoint;
+
+
+
+    private void Update()
+    {
+        CreateCubeOnClick();
+        //Debug.Log(lastEndPoint);
+    }
+
+
+    /// <summary>
+    /// This method is for creating new cubes when pressing the mouse button
+    /// </summary>
+    private void CreateCubeOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject currentCube = Instantiate(cubePrefab, lastEndPoint, Quaternion.identity);
+            Cube cubeScript = currentCube.GetComponent<Cube>();
+            lastEndPoint = cubeScript.ReturnEndPoint();
+        }
+    }
+
+
+
+}
