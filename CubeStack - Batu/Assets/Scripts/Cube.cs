edbc002a6c122b054ddee5f6e7cd86c0ba097fe3
Ziya <47672872+SafSafir@users.noreply.github.com>
2021-01-27 21:55:18 +0300

    Herkese Özel Proje Alanı Oluşturuldu
    Artık Herkes Kendi Alanında Çalışma Yapacak

---
+++ CubeStack - Batu/Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+
+    /// <summary>
+    /// This method is for detecting the last end point
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 ReturnEndPoint()
+    {
+
+        Vector3 CalculatedEndPoint = transform.position + Vector3.up;
+
+        return CalculatedEndPoint;
+    }
+}
